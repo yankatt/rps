545c50b4236ae91c5872e01b5e6511ffe9a25a56
Yannic Kattwinkel <yannic.kattwinkel@proximity.de>
2023-05-01 12:03:35 +0200

    add function to handle user input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
-import { input } from '@inquirer/prompts';
+import { input, select } from '@inquirer/prompts';
+import handleSignInput from './functions/handleSignInput.js';
 
 let scores = {
   player: 0,
@@ -8,3 +9,29 @@ let scores = {
 }
 
 const playerName = await input({ message: 'Enter your name' });
+
+while(scores.player < 3 && scores.npc < 3) {
+  const sign = await select({
+    message: 'Select a sign',
+    choices: [
+      {
+        name: 'rock',
+        value: 'rock',
+        description: 'solid rock! never a bad choice!',
+      },
+      {
+        name: 'paper',
+        value: 'paper',
+        description: 'soft and smooth! do not underestimate it!',
+      },
+      {
+        name: 'scissors',
+        value: 'scissors',
+        description: 'sharp like a razorblade!',
+      },
+    ],
+  });
+
+  handleSignInput(sign)
+  scores.npc++;
+}
